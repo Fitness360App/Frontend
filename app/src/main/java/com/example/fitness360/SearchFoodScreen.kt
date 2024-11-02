dd0de9cfbd73f34f16639ba9d96af946fdc0d495
Acaymo Granado Sánchez <acaimopcgc@gmail.com>
2024-11-02 21:48:38 +0100

    pantallas de busqueda

--- app/src/main/java/com/example/fitness360/SearchFoodScreen.kt
+++ app/src/main/java/com/example/fitness360/SearchFoodScreen.kt
@@ -18,7 +18,9 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
@@ -31,73 +33,135 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 
 import com.example.fitness360.components.BottomNavigationBar
+import com.example.fitness360.components.ProductCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchFoodScreen(navController: NavController) {
-    Column(
+    val match = remember { mutableStateOf(true) }
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 0.dp),
-        horizontalAlignment = Alignment.Start
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 0.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 2.dp)
+                .fillMaxSize()
+                .padding(bottom = 56.dp) // Espacio reservado para el BottomNavigationBar
         ) {
-            TextField(
-                value = "",
-                onValueChange = {},
-                placeholder = {
-                    Text(
-                        text = "Buscar Producto",
-                        color = Color.Gray,
-                        fontSize = 13.sp
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 2.dp)
+            ) {
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(
+                            text = "Buscar Producto",
+                            color = Color.Gray,
+                            fontSize = 13.sp
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Buscar",
+                            tint = Color.Gray,
+                            modifier = Modifier
+                                .size(24.dp)
+                                .clickable {
+                                    match.value = false // Al hacer clic, se establece match en false
+                                }
+                        )
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(56.dp)
+                        .background(color = Color(0xFFE4E3E3), shape = RoundedCornerShape(8.dp)),
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Box(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .background(color = Color(0xFFE4E3E3), shape = RoundedCornerShape(8.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.CameraAlt,
+                        contentDescription = "Escanear",
+                        tint = Color.Gray,
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clickable {
+                                match.value = true // Al hacer clic, se establece match en false
+                            }
                     )
-                },
-                trailingIcon = {
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (match.value) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(5) { // Número de ProductCards a mostrar
+                        ProductCard()
+                    }
+                }
+            } else {
+                // Pantalla de "No se han encontrado resultados"
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Icon(
                         imageVector = Icons.Filled.Search,
-                        contentDescription = "Buscar",
+                        contentDescription = "No se encontraron resultados",
                         tint = Color.Gray,
-                        modifier = Modifier.size(24.dp)
-
+                        modifier = Modifier.size(64.dp)
                     )
-                },
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                ),
-                modifier = Modifier
-                    .weight(1f)
-                    .height(56.dp) // Altura ajustada para que coincida con el Box de la cámara
-                    .background(color = Color(0xFFE4E3E3), shape = RoundedCornerShape(8.dp)),
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            // Caja que contiene el icono de la cámara
-            Box(
-                modifier = Modifier
-                    .size(56.dp) // Tamaño del contenedor de la cámara para que coincida con la altura del TextField
-                    .background(color = Color(0xFFE4E3E3), shape = RoundedCornerShape(8.dp)),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.CameraAlt,
-                    contentDescription = "Escanear",
-                    tint = Color.Gray,
-                    modifier = Modifier.size(24.dp) // Tamaño del icono dentro de la caja
-                )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "No se han encontrado resultados",
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
             }
         }
 
-        BottomNavigationBar(navController)
+        // Agregar el BottomNavigationBar sin el modifier, en la parte inferior del Box
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            BottomNavigationBar(navController)
+        }
     }
 }
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/fitness360/components/productCard.kt
@@ -0,0 +1,247 @@
+package com.example.fitness360.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fitness360.R // Ajusta este import a tu proyecto
+
+@Composable
+fun ProductCard() {
+    var showDialog by remember { mutableStateOf(false) } // Controla la visibilidad del cuadro de diálogo
+
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Card(
+            shape = RoundedCornerShape(10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF005A9E)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.product_image),
+                        contentDescription = "Producto",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .background(Color.White, shape = RoundedCornerShape(10.dp))
+                            .padding(8.dp)
+                    )
+
+                    // Botón de añadir
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 8.dp, end = 8.dp)
+                            .size(32.dp)
+                            .background(Color(0xFFFFA000), shape = RoundedCornerShape(6.dp))
+                            .clickable { showDialog = true },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Añadir",
+                            tint = Color.White
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Información nutricional
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            NutritionalInfo(label = "Carbohidratos", value = "50gr/100gr")
+                            Spacer(modifier = Modifier.height(8.dp))
+                            NutritionalInfo(label = "Kilocalorías", value = "125 kcals/100gr")
+                        }
+
+                        Spacer(modifier = Modifier.width(32.dp))
+
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            NutritionalInfo(label = "Grasas", value = "25gr/100gr")
+                            Spacer(modifier = Modifier.height(8.dp))
+                            NutritionalInfo(label = "Proteínas", value = "30gr/100gr")
+                        }
+                    }
+                }
+            }
+        }
+
+        // Muestra el diálogo sobre la tarjeta cuando showDialog es true
+        if (showDialog) {
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .clip(RoundedCornerShape(10.dp)), // Aplica la forma redondeada
+                contentAlignment = Alignment.Center
+            ) {
+                AddToMealDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { mealType, quantity ->
+                        // Aquí puedes manejar la lógica para añadir la cantidad a la comida seleccionada
+                        showDialog = false
+                    }
+                )
+            }
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddToMealDialog(onDismiss: () -> Unit, onConfirm: (String, String) -> Unit) {
+    var selectedMeal by remember { mutableStateOf("Desayuno") }
+    var quantity by remember { mutableStateOf("") }
+    val mealOptions = listOf("Desayuno", "Almuerzo", "Merienda", "Cena")
+    var expanded by remember { mutableStateOf(false) }
+
+    // Fondo oscuro y diálogo en el centro de la tarjeta
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.5f))
+            .clickable(onClick = onDismiss),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.8f) // Ajusta el ancho del diálogo
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+                .padding(16.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Añadir a la comida",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF005A9E)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Selector de tipo de comida
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { expanded = !expanded }
+                        .background(Color.LightGray, shape = RoundedCornerShape(4.dp))
+                        .padding(12.dp)
+                ) {
+                    Text(selectedMeal)
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    mealOptions.forEach { meal ->
+                        DropdownMenuItem(
+                            text = { Text(meal) },
+                            onClick = {
+                                selectedMeal = meal
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Campo de texto para la cantidad
+                OutlinedTextField(
+                    value = quantity,
+                    onValueChange = { quantity = it },
+                    label = { Text("Cantidad") },
+                    placeholder = { Text("Escribe la cantidad a añadir") },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFF005A9E),
+                        cursorColor = Color(0xFF005A9E)
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                // Botones de acción
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    TextButton(onClick = onDismiss) {
+                        Text("Cancelar", color = Color.Gray)
+                    }
+                    Button(
+                        onClick = { onConfirm(selectedMeal, quantity) },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF005A9E))
+                    ) {
+                        Text("Añadir", color = Color.White)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NutritionalInfo(label: String, value: String) {
+    Column(horizontalAlignment = Alignment.Start) {
+        Text(
+            text = label,
+            color = Color.White,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = value,
+            color = Color.White.copy(alpha = 0.7f),
+            fontSize = 12.sp
+        )
+    }
+}
